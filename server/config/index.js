f28cc102cb2c97068ee3fa2c864864cb8226b35a
Ivan Poliarush <56694325+ipoliarush@users.noreply.github.com>
2021-06-28 08:23:29 +0300

    Create index.js

---
+++ server/config/index.js
@@ -0,0 +1,10 @@
+const 
+  // secret = require('./jwtsecret'),
+  // email = require('./jwtsecretemail'),
+  uri = require('./mongodb')
+
+module.exports = {
+  // secret: secret,
+  // emailsecret: email,
+  uri: uri
+}
