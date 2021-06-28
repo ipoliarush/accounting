5862393c478972ce050ef5cb275418467c60072b
Ivan Poliarush <56694325+ipoliarush@users.noreply.github.com>
2021-06-28 07:36:47 +0300

    Update product.js

--- server/models/product.js
+++ server/models/product.js
@@ -3,7 +3,13 @@ const { Schema, model } = require('mongoose')
 const ProductSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
+  },
+  vendor_code: {
+    type: Number,
+    required: true,
+    trim: true
   },
   stock: {
     type: Number,
@@ -12,12 +18,30 @@ const ProductSchema = new Schema({
   delivered: {
     type: Number,
     default: 0
-  }
+  },
+  reserved: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    get: getPrice, 
+    set: setPrice
+  },
 }, {
   timestamps: {
     createdAt: 'createdAt',
     updatedAt: 'updatedAt'
   }
 })
+
+getPrice(num => {
+  (num / 100).toFixed(2)
+})
+
+setPrice(num => {
+  num * 100
+})
+
 
 module.exports = model('Product', ProductSchema)
