4b20454e5ec73372b5a3487d9372ec5e0e7eb992
Ivan Poliarush <56694325+ipoliarush@users.noreply.github.com>
2021-07-17 07:23:29 +0300

    Delete server directory

--- server/config/mongodb.js
+++
@@ -1,7 +0,0 @@
-const 
-  user = 'ipol',
-  password = '1q2w3e4r5t',
-  dbname = 'accountingdb',
-  uri = `mongodb+srv://${user}:${password}@cluster0.6jukp.mongodb.net/${dbname}`
-
-module.exports = uri
